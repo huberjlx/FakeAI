a7a3030c450c44b26c5fd10e97838acc01887d04
Lord-Lelsers <millan.kumar.24@bishops.com>
2021-10-07 09:23:24 -0700

    Started the non linear correlation based learning; still not sure the activation or square rooting works?

---
+++ 46731DepthLearning.py
@@ -0,0 +1,156 @@
+import random
+import math
+
+class Data():
+
+    def __init__(self, info):
+        self.info = info
+
+    def readInInfo(self, fname):
+        print("not done yet")
+    def getInfo(self):
+        return self.info
+    def getRows(self): # total # how many tall
+        return len(self.info)
+    def getColumns(self): # rows # how many on wide
+        return len(self.info[0])
+    def getValueAt(self, rowNum, colNum):
+        return self.info[rowNum][colNum]
+    def getResultForRow(self, rowNum):
+        return self.info[rowNum][len(self.info[rowNum]) - 1]
+
+def swish(x):
+    return x  * ((1 + math.exp(-1)) ** -1)
+
+class Layer():
+
+    def __init__(self, inPoints, outPoints, layerNum):
+        self.outPoints = outPoints
+        self.inPoints = inPoints
+        self.layerNum = layerNum
+        self.weights = []
+        for points in range(self.outPoints):
+            temp = []
+            for weight in range(self.inPoints):
+                w = random.random()
+                temp.append(w)
+            self.weights.append(temp)
+
+    def calcLayer(self, inputs):
+        sums = []
+        for outPoint in range(self.outPoints):
+            sum = 0
+            for point in range(len(inputs)): # len(inputs) should be euqal to inPoints
+                sum = sum + self.weights[outPoint][point] * inputs[point]
+            sum = swish(sum) # ACTIVATE
+            sums.append(sum)
+        return sums
+
+    def updateWeights(self, learningRate, dif, target):
+        for points in range(self.outPoints):
+            for weight in range(self.inPoints):
+                self.weights[points][weight] = self.weights[points][weight] - learningRate * (dif * target)
+
+    def printWeights(self):
+        title = "layer%i  :  (%i in, %i out)\n" % (self.layerNum, self.inPoints, self.outPoints)
+        weightsString = ""
+        for point in range(self.outPoints):
+            for weight in range(self.inPoints):
+                weightsString  = "%s%.3f " % (weightsString, self.weights[point][weight])
+            weightsString = weightsString + "\n"
+        finalString = title + weightsString
+        print(finalString)
+        return finalString
+            
+
+
+class AI():
+
+    def __init__(self, layers):
+        self.learningRate = 0.001
+        self.data = []
+        self.layers = layers
+
+
+    def learn(self, numberOfTests):
+
+        for cycle in range(numberOfTests):
+            print("\nCycle: %i" % cycle)
+
+            for rowNum in range(self.data.getRows()):
+                inputs = []
+                for colNum in range(self.data.getColumns() - 1):
+                    inputs.append(self.data.getValueAt(rowNum, colNum))
+                for layer in self.layers:
+                    inputs = layer.calcLayer(inputs)
+                sum = inputs[0]
+
+                # WHY IS THIS NEEDED???
+                try:
+                    sum = math.sqrt(sum)
+                except:
+                    sum = -(math.sqrt(-sum))
+
+                dif = sum - self.data.getResultForRow(rowNum)
+                print("DIF: %f" % dif)
+                for layer in self.layers:
+                    layer.updateWeights(self.learningRate, dif, self.data.getResultForRow(rowNum))
+
+            self.printWeights()
+                
+
+    def predictRow(self, row):
+        inputs = []
+        for colNum in range(self.data.getColumns() - 1):
+            inputs.append(row[colNum])
+        for layer in self.layers:
+            inputs = layer.calcLayer(inputs)
+        sum = inputs[0]
+        
+        value = round(sum)
+        print("Sum: %f" % sum)
+        print("Value: %i" % value)
+        return value
+            
+    def printWeights(self):
+        for layer in self.layers:
+            layer.printWeights()
+        print("\n")
+
+    def setData(self, data):
+        self.data = data
+    def getData(self):
+        return self.data
+
+
+def main():
+
+    l0 = Layer(4, 6, 0)
+    l1 = Layer(6, 7, 1)
+    l2 = Layer(7, 3, 2)
+    l3 = Layer(3, 1, 0)
+    layers = [l0, l1, l2, l3]
+    ai = AI(layers)
+    info = [
+        [1, 0, 1, 1, 1],
+        [1, 0, 0, 1, 1],
+        [0, 1, 1, 0, 0],
+        [1, 1, 0, 0, 1],
+        [0, 1, 0, 1, 0],
+        [0, 0, 1, 1, 0]
+    ]
+    data = Data(info)
+    ai.setData(data)
+    ai.printWeights()
+    temp = input("Press [enter] to start learning: ")
+    ai.learn(999)
+    print("\n\nFinal Weights")
+    ai.printWeights()
+
+    row = [1, 0, 1, 0] # should be 1
+    print("\n\nTrying to predict the result if this was the data: ")
+    print(row)
+    print("\nPrediction: ")
+    ai.predictRow(row)
+
+main()
